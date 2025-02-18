f2e79bc4a7629830935201082a37fd7dd92acacd
iKeiNagai <thomasvasquez68@gmail.com>
2025-02-18 18:36:25 -0500

    project setup

---
+++ flick-review/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+//simple get request
+app.get('/', (req, res) =>{
+    res.send("Get request")
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}`)
+})
