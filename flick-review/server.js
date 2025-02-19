73eb683ade45b45622e8b445cd86b449413738cf
Nikali Wood <167284895+nikaliwood@users.noreply.github.com>
2025-02-19 17:39:45 -0500

    Update server.js

--- flick-review/server.js
+++ flick-review/server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const admin = require("firebase-admin");
+const path = require("path");
+require("dotenv").config();
 
+
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
+
+//initialize firebase admin sdk with credentials
+admin.initializeApp({
+  credential: admin.credential.cert(require(path.resolve(serviceAccountPath))),
+});
 const app = express();
 
 const PORT = 3000;
