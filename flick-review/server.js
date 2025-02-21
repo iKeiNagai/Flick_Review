baa9df4b4b00331c77ce135ca49c7de87ccbf102
iKeiNagai <thomasvasquez68@gmail.com>
2025-02-21 15:00:03 -0500

    chore: installed ejs template engine

--- flick-review/server.js
+++ flick-review/server.js
@@ -21,14 +21,14 @@
 // });
 
 const express = require("express");
-const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-// Serve the HTML file from the same folder as server.js
+app.set('view engine', 'ejs')
+//
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.render("index");
 });
 
 app.listen(PORT, () => {
