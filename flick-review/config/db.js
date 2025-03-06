0d3f037ebe8415280057648ebedc4beabaa68e3b
Nikali Wood <nikalicwood@gmail.com>
2025-03-05 22:59:01 -0500

    Added login and signup page and configured it to firebase

--- flick-review/config/db.js
+++ flick-review/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",           // Matches settings.json
-    password: "Password",   // Matches settings.json
+    password: "",   // Matches settings.json
     database: "Flickreview", // Ensure it matches the correct database name
     port: 3306              // Matches settings.json
 });
