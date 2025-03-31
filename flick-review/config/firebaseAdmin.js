0563c5936edba840b9c9e8b0971e0be2c9dd19f5
BhaskarTej <bhaskarteja02@gmail.com>
2025-03-31 00:32:24 -0400

    Changed some Auth stuff and cleaned up.

--- flick-review/config/firebaseAdmin.js
+++ flick-review/config/firebaseAdmin.js
@@ -1,8 +1,18 @@
+require('dotenv').config();
 const admin = require("firebase-admin");
+
+// Ensure the path to your JSON file is provided
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH) {
+  throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_KEY_PATH environment variable.");
+}
+
+// Load the service account key JSON file
 const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
+
+console.log("Firebase initialized successfully.");
 
 module.exports = admin;
